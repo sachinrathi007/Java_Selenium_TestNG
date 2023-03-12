539eb4f3e240de024db63e7463a31a608e0b1c83
sachinrathi007 <yappi.sachin@gmail.com>
2023-03-13 00:16:45 +0530

    Initial Commit
    Initial Commit

---
+++ MasterSeleniumFramework-master/src/test/java/org/selenium/tests/NavigationTest.java
@@ -0,0 +1,34 @@
+
+
+/***************************************************/
+
+package org.selenium.tests;
+
+import org.selenium.annotations.FrameworkAnnotation;
+import org.selenium.base.BaseTest;
+import org.selenium.enums.AuthorType;
+import org.selenium.enums.CategoryType;
+import org.selenium.pages.HomePage;
+import org.selenium.pages.StorePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NavigationTest extends BaseTest{
+
+	@FrameworkAnnotation(author = { AuthorType.ABC, AuthorType.ABC},
+			category = { CategoryType.BVT,CategoryType.REGRESSION })
+	@Test(groups = {"BVT","REGRESSION"})
+	public void navigateFromHomeToStoreUsingMainMenu() {
+		
+		StorePage storePage =
+				new HomePage(getDriver()).
+					load().
+					getMyHeader().
+					navigateToStoreUsingMenu();
+		
+		Assert.assertEquals(storePage.getTitle(), "Store");
+
+	}
+	
+	
+}
